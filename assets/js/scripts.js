24c32f5818b38bb56d7cc7d8830aa4f0f3f7422f
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-04-04 19:18:17 +0000

    caching the dom, creating variables for the buttons rock, paper, scissors and the score count

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,9 @@ const playerScore = 0;
 const compScore = 0;
 const playerScore_div = document.getElementById("player-score"); //_div to show its in a div
 const compScore_div = document.getElementById("comp-score");
+const scoreBox_div = document.querySelector(".score-box");
+const result_div = document.querySelector(".result");
+const rock_button = document.getElementById("rock");
+const paper_button = document.getElementById("paper");
+const scissors_button = document.getElementById("scissors");
+
