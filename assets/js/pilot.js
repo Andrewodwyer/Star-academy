1b6bb5f4f257d52c0538516d1e3c07f0891881b9
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-04-15 09:59:35 +0000

    js file was placed on the wrong html page causing error, changed back

--- assets/js/pilot.js
+++ assets/js/pilot.js
@@ -1,16 +1,15 @@
 //HAMBURGER MENU
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-hamburger.addEventListener("click", navBar() {
+hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active"); 
     navMenu.classList.toggle("active"); 
-});
-
+})
 document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click", navBar() {
+    addEventListener("click", function() {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
-    }));
+    }))
 
 //Pilot Select
 //getters
